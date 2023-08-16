602dd2202e81bfd492d307281844414fbc6d4043
typed <gxj21720@126.com>
2023-08-16 14:13:26 +0800

    1

--- test.cpp
+++ test.cpp
@@ -8,10 +8,11 @@ int main()
 {
     uint64_t compressCount = 0;
     uint64_t outCount = 0;
+    const uint64_t outMax = 100000000;
     while (true) {
         test_compress_texture();
         ++compressCount;
-        if (++outCount >= 10000000) {
+        if (++outCount >= outMax) {
             outCount = 0;
             cout << "test_compress_texture compressCount:" << compressCount << endl;
         }
