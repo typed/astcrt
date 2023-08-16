8a1e405f04c1772e2aeb95e455682c20164aec23
typed <gxj21720@126.com>
2023-08-16 14:06:58 +0800

    1

--- test.cpp
+++ test.cpp
@@ -6,14 +6,12 @@ using namespace std;
 
 int main()
 {
+    uint64_t compressCount = 0;
+    uint64_t outCount = 0;
     while (true) {
-        static uint64_t compressCount = 0;
-        static int32_t outCount = 0;
-        for (int32_t i = 0; i < 1000; ++i) {
-            test_compress_texture();
-            ++compressCount;
-        }
-        if (++outCount >= 1000) {
+        test_compress_texture();
+        ++compressCount;
+        if (++outCount >= 10000000) {
             outCount = 0;
             cout << "test_compress_texture compressCount:" << compressCount << endl;
         }
