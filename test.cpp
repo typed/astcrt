3a177b95a99a06fe7bccacf11503ba1c657830a3
typed <gxj21720@126.com>
2023-08-16 14:23:06 +0800

    1

--- test.cpp
+++ test.cpp
@@ -1,20 +1,21 @@
 ﻿#include "compress_texture.h"
 #include <iostream>
 #include <cstdint>
+#include <time.h>
 
 using namespace std;
 
 int main()
 {
     uint64_t compressCount = 0;
-    uint64_t outCount = 0;
-    const uint64_t outMax = 100000000;
+    time_t lastTime = time(0);
     while (true) {
         test_compress_texture();
         ++compressCount;
-        if (++outCount >= outMax) {
-            outCount = 0;
-            cout << "test_compress_texture compressCount:" << compressCount << endl;
+        time_t nowTime = time(0);
+        if (nowTime > lastTime) {
+            lastTime = nowTime;
+            cout << "test_compress_texture compressCount:" << compressCount << "\r";
         }
     }
     return 0;
